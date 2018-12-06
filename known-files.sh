ac43459b598ef550fef0533884e1646641fc99cb
n89cz <dusek@n89.cz>
2018-12-06 14:01:47 +0100

    send mail with unknown files

--- known-files.sh
+++ known-files.sh
@@ -26,21 +26,28 @@ fi
 
 #pokud vychozi seznam existuje udelame soucasny stav
 if [ -f $KFILE  ]; then
-    echo "udelej soucasny stav"
-    find $TESTF > $CFILES
+    find $TESTF > $CFILES-$NOW
 else
     echo "Known file list not existing"
     exit 1
 fi
 
+#compare known file list with current file list
+
+diff $KFILES $CFILES-$NOW >> $UKFILES-$NOW
+
+if [ $? -ne 0 ];then
+    echo "poslat upozorneni se seznamem na email a ulozime do logu"
+    cat $UKFILES-$NOW | mail -s "unknow files detected!" recipient@site.com
+    exit 2
+fi
 
 
 
-#porovname vychozi a soucasny stav
+
 
 #pokud nic nenelazeno exit 0
 
 #pokud nalezneme neznamy soubor dame jej do seznamu neznamych souboru (vcetne cele cesty)
 
-#nakonec seznam posleme emailem a prejmenujeme ho (napr. unknow.list-2018-12-05
-
+#nakonec seznam posleme emailem a konec
